2713e12f234a85ac50fb030d54a284d3d649d106
imuuu <33964608+imuuu@users.noreply.github.com>
2017-12-10 17:17:12 +0200

    Add files via upload

---
+++ WhisperingTestinghomee.sql
@@ -0,0 +1,25 @@
+-- --------------------------------------------------------
+-- Verkkotietokone:              127.0.0.1
+-- Palvelinversio:               10.2.10-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Versio:              9.4.0.5125
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Tietojen vientiä ei oltu valittu.
+-- Tietojen vientiä ei oltu valittu.
+-- Tietojen vientiä ei oltu valittu.
+-- Tietojen vientiä ei oltu valittu.
+-- Tietojen vientiä ei oltu valittu.
+-- Tietojen vientiä ei oltu valittu.
+-- Tietojen vientiä ei oltu valittu.
+-- Tietojen vientiä ei oltu valittu.
+-- Tietojen vientiä ei oltu valittu.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
